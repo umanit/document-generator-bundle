a22935c898449efd6accf369d8fa52d17726278d
DjLeChuck <djlechuck@gmail.com>
2019-07-28 14:20:41 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# UmanIT - Document Generator Bundle
+
+This bundle is used to drive the document generator microservice developed by UmanIT:
+https://github.com/umanit/microservice-document-generator
---
+++ src/UmanitDocumentGeneratorBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Umanit\DocumentGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class UmanitDocumentGeneratorBundle
+ */
+class UmanitDocumentGeneratorBundle extends Bundle
+{
+}
